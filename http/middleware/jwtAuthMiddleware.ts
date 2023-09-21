40a063d23efec8dcf220504f9fe3a70cf5e5ac22
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2023-09-21 20:39:10 +0500

    removed mlogs

--- http/middleware/jwtAuthMiddleware.ts
+++ http/middleware/jwtAuthMiddleware.ts
@@ -1,15 +1,10 @@
 import AuthInfraService from "../../src/infrastructure/services/auth/auth.infra";
-import {GoogleOAuth2Infra} from "../../src/infrastructure/services/thirdparty/googleOAuth2.infra";
 
 const jwtAuthMiddleWare = (request: any, response: any, next: any) => {
     const authInfra = new AuthInfraService();
-    const oAuth2Infra = new GoogleOAuth2Infra();
     const jwtToken = request.get("Authorization")?.slice(7);
     try {
-        const tokenContents: any = authInfra.verifyJwtToken(jwtToken);
-        if (tokenContents.oAuthRefreshToken) {
-            console.log();
-        }
+        authInfra.verifyJwtToken(jwtToken);
 
         next();
     } catch (error) {
